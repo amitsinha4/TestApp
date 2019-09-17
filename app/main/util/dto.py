bfbe34e0997aad3605aa2225e498e05f5a51279c
Amit Sinha <amit.sinha@thepsi.com>
2019-09-17 17:09:33 +0530

    Swagger Improved

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -6,7 +6,7 @@ class UserDto:
     """ UserDto """
     api = Namespace('user', description='user related operations')
 
-    user = api.model('user_data', {
+    user = api.model('user', {
         'email': fields.String(required=True, description='user email address'),
         'username': fields.String(required=True, description='user username'),
         # 'password': fields.String(required=True, description='user password'),
